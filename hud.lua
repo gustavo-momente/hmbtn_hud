b166173eb312c5a1642ceac158ce9f15ecd32f47
Gustavo Momente <momente.gustavo@gmail.com>
2016-12-22 10:13:02 -0200

    Now displaying stamina/fatigue

--- hud.lua
+++ hud.lua
@@ -1,9 +1,13 @@
-local FatigueAddr = 0x071A14
-local Fatigue
+local staminaAddr = 0x071A12
+local fatigueAddr = 0x071A16
+local lineHeight = 16
 
 while true do
-	gui.text(0,20,"Fatigue: " .. FatigueAddr)
-	Fatigue = memory.readword(FatigueAddr)
-	gui.text(0,0,"Fatigue: " .. Fatigue)
+	stamina = memory.read_s16_le(staminaAddr)
+	fatigue = memory.read_s16_le(fatigueAddr)
+
+	strFtgStr = string.format("STM/FTG: %03d/%03d", stamina, fatigue)
+	gui.text(0, lineHeight, strFtgStr)
+	-- gui.text(0, 2 * lineHeight, "Fatigue: " .. fatigue, nil, nil, fontSize)
 	emu.frameadvance()
 end
